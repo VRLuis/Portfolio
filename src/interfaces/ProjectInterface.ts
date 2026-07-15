52f4349df367ef43a53d7c0d189a86c36216df9d
VRLuis <luisvillarroldan1010@gmail.com>
2026-07-15 18:45:34 +0200

    add: project section done

---
+++ src/interfaces/ProjectInterface.ts
@@ -0,0 +1,9 @@
+interface ProjectInterface {
+    projectName: string;
+    projectDescription: string;
+    projectImage: string;
+    projectLink: string;
+    projectWeb: string;
+    projectTechnologies: string[];
+    inProgress: boolean;
+}
